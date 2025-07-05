cfab0b370a1b7516b3a3d413fee81fc5a0e8ba35
mohdnas0001 <mohdnas0001@gmail.com>
2025-07-05 11:10:27 +0100

    feat: added swagger docs

--- src/swagger.js
+++ src/swagger.js
@@ -31,8 +31,8 @@ const options = {
     },
   },
   apis: [
-    './src/domain/auth/routes/authRoutes.js',
-    './src/domain/game/routes/gameRoutes.js',
+    './domain/auth/routes/authRoutes.js',
+    './domain/game/routes/gameRoutes.js',
   ],
 };
 
